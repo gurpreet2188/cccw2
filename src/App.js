930c7aa330f6c2cf8fd1a10d65b21cdda676d667
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2022-08-08 18:14:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const f = async () => {
+      const res = await fetch('https://google-finance4.p.rapidapi.com/market-trends/?t=most-active&hl=en&gl=SG' , {
+        headers:{
+          'X-RapidAPI-Key': 'f12a5d921emshbac7e919797abd5p1b88ecjsn6a241dfe72bb',
+		      'X-RapidAPI-Host': 'google-finance4.p.rapidapi.com'
+        }
+      })
+
+      const d = res.json()
+      console.log(d)
+    }
+    f()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">

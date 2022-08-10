e8f020f6ab95a6895cffaf6b17b09c3b6249b560
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2022-08-10 12:16:20 +0800

    added crypto file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import Crypto from './data/crpyto';
+
 
 function App() {
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    const f = async () => {
-      const res = await fetch('https://google-finance4.p.rapidapi.com/market-trends/?t=most-active&hl=en&gl=SG' , {
-        headers:{
-          'X-RapidAPI-Key': 'f12a5d921emshbac7e919797abd5p1b88ecjsn6a241dfe72bb',
-		      'X-RapidAPI-Host': 'google-finance4.p.rapidapi.com'
-        }
-      })
-
-      const d = res.json()
-      console.log(d)
-      console.log(d)
-    }
-    f()
-  }, [])
+  const cryptoData = Crypto()
+  console.log(cryptoData)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

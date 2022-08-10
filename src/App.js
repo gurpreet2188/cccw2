c76528e3a6bc3ce56ec633cba5a266322da01df3
Hyunjae Kim <hjkim6321@gmail.com>
2022-08-10 15:15:03 +0800

    Commit 3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import './App.css';
-import Crypto from './data/crpyto';
-
+import "./App.css";
+import Crypto from "./data/crpyto";
 
 function App() {
-  const [data, setData] = useState()
+  const cryptoData = Crypto();
+  console.log(cryptoData);
 
-  useEffect(() => {
-    const f = async () => {
-      const res = await fetch('https://google-finance4.p.rapidapi.com/market-trends/?t=most-active&hl=en&gl=SG' , {
-        headers:{
-          'X-RapidAPI-Key': 'f12a5d921emshbac7e919797abd5p1b88ecjsn6a241dfe72bb',
-		      'X-RapidAPI-Host': 'google-finance4.p.rapidapi.com'
-        }
-      })
-
-      const d = res.json()
-      console.log(d)
-      console.log(d)
-    }
-    f()
-  }, [])
-
-  return (
-    <div className="App">
-    </div>
-  );
-}
-
-function IsLeapYear(year)
-{
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-    {
-        return 1;
-    }
-    return 0;
+  return <div className="App"></div>;
 }
 
 export default App;

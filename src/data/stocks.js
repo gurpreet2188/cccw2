02abe2e7f171a6daea6c6cc0a56ec39dc11aa834
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2022-08-10 12:19:32 +0800

    stocks branch

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export default function Crpyto() {
+export default function stocks() {
     const [data, SetData] = useState()
 
     useEffect(() => {

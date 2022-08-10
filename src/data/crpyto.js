b74e58d233d4240ac09ebda27d45f7052637adf8
Hyunjae Kim <hjkim6321@gmail.com>
2022-08-10 17:12:33 +0800

    test

--- src/data/crpyto.js
+++ src/data/crpyto.js
@@ -2,15 +2,41 @@ import { useEffect, useState } from "react"
 
 export default function Crpyto() {
     const [data, SetData] = useState()
+    //current price
+    let ids = 'bitcoin'
+    let vs_currencies = 'usd'
+    let currentPriceURL = 'https://api.coingecko.com/api/v3/simple/price?ids=' + ids + '&vs_currencies=' + vs_currencies
+
+    //historical data
+    let day = 30
+    let month = 12
+    let year = 2017
+    let historicalDataURL = 'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=' + day.toString() + '-' + month.toString() + '-' + year.toString();
 
     useEffect(() => {
         const f = async () => {
-            const res = await fetch('')
+            const res = await fetch(currentPriceURL)
             const d = await res.json()
             SetData(d)
         }
         f()
     }, [])
+
+    // return (
+    //     <div className="Crypto">
+    //     {data?.entries?.map((v, i) => {
+    //     return(
+    //      <>
+    //       <p>
+    //         {
+    //           v.bitcoin.usd
+    //         }
+    //       </p>
+    //      </>  
+    //     )
+    //     })}
+    //     </div>
+    // );
 
     return data
 }

f8eda1524e4c7e5597829400ea07837fa3e42f09
Hyunjae Kim <hjkim6321@gmail.com>
2022-08-11 14:52:18 +0800

    past price and trending coins

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Crypto from "./data/crpyto";
 function App() {
   const cName = 'bitcoin'
   const cryptoData = Crypto(cName);
-  const crypObjUSD = cryptoData ? Object.values(cryptoData)[0]?.usd : 'none'
-  // console.log(crypObjName);
+  const cCurrentData = cryptoData[0];
+  const cHistoricalData = cryptoData[1];
+  const cTrendingData = cryptoData[2];
+  const currentPrice = cCurrentData ? Object.values(cCurrentData)[0]?.usd : 'none'
+  const pastPrice = cHistoricalData ? Object.values(cHistoricalData)[4]?.market_data?.current_price?.usd : 'none'
+  //const profitRate = (currentPrice - pastPrice) / pastPrice * 100;
+  console.log(cCurrentData)
+  console.log(cHistoricalData)
+  console.log(cTrendingData)
 
   return <div className="App">
 
     <h1>{cName}</h1>
-    <p>value: {crypObjUSD}</p>
+    <p>current price: {currentPrice}</p>
+    <p>past price: {pastPrice}</p>
   </div>;
 }
 
--- src/data/crpyto.js
+++ src/data/crpyto.js
@@ -1,25 +1,48 @@
 import { useEffect, useState } from "react"
 
 export default function Crpyto(cryptoName) {
-    const [data, SetData] = useState()
+    const [currentPrice, SetCurrentPrice] = useState()
+    const [historicalData, SetHistoricalData] = useState()
+    const [trendingCoins, SetTrendingCoins] = useState()
     //current price
-    let vs_currencies = 'usd'
-    let currentPriceURL = 'https://api.coingecko.com/api/v3/simple/price?ids=' + cryptoName + '&vs_currencies=' + vs_currencies
+    const vs_currencies = 'usd'
+    const currentPriceURL = 'https://api.coingecko.com/api/v3/simple/price?ids=' + cryptoName + '&vs_currencies=' + vs_currencies
 
     //historical data
-    let day = 30
-    let month = 12
-    let year = 2017
-    let historicalDataURL = 'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=' + day.toString() + '-' + month.toString() + '-' + year.toString();
+    const currentDate = new Date()
+    const previousDate = currentDate.setDate(currentDate.getDate() - 1)
+    let day = new Date(previousDate).getDate() 
+    let month = new Date(previousDate).getMonth() 
+    let year = new Date(previousDate).getFullYear()
+    console.log(year)
+
+    const historicalDataURL = 'https://api.coingecko.com/api/v3/coins/' + cryptoName + '/history?date=' + day + '-' + month + '-' + year;
+    
+    //trending coins
+    const trendingURL = 'https://api.coingecko.com/api/v3/search/trending'
+
+    //market cap
 
     useEffect(() => {
-        const f = async () => {
+        const f1 = async () => {
             const res = await fetch(currentPriceURL)
             const d = await res.json()
-            SetData(d)
+            SetCurrentPrice(d)
         }
-        f()
+        f1()
+        const f2 = async () => {
+            const res = await fetch(historicalDataURL)
+            const d = await res.json()
+            SetHistoricalData(d)
+        }
+        f2()
+        const f3 = async () => {
+            const res = await fetch(trendingURL)
+            const d = await res.json()
+            SetTrendingCoins(d)
+        }
+        f3()
     }, [])
 
-    return data
+    return [currentPrice, historicalData, trendingCoins]
 }

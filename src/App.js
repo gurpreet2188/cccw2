33a8c27c970cc5aa54fa8e4e55b7a10ff43751cb
Hyunjae Kim <hjkim6321@gmail.com>
2022-08-16 14:48:17 +0800

    maket cap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,42 @@ import "./App.css";
 import Crypto from "./data/crpyto";
 
 function App() {
-  const cName = 'bitcoin'
-  const [cCurrentData, cHistoricalData, cTrendingData] = Crypto(cName)
-  const currentPrice = parseInt(cCurrentData ? Object.values(cCurrentData)[0]?.usd : 'none')
-  const pastPrice = parseInt(cHistoricalData?.market_data?.current_price?.usd) 
-  const profitRate = (currentPrice - pastPrice) / pastPrice * 100;
-  console.log(cCurrentData)
-  console.log(cHistoricalData)
-  console.log(cTrendingData?.coins[0]?.item?.name)
-
-  return <div className="App">
-
-    <h1>{cName}</h1>
-    <p>current price: {currentPrice}</p>
-    <p>profit rate: {(profitRate).toFixed(2)}%</p>
-    <h2>top 7 trending coins</h2>
-    {cTrendingData ? cTrendingData?.coins.map((v, i) => {
-      return(<p>{i + 1}. {v?.item?.name}</p>)
-    }) : 'Loading...'}
-  </div>;
+  const cName = "bitcoin";
+  const [cCoinData, cTrendingData, cMarketCapData] = Crypto(cName);
+  console.log(cCoinData)
+  const currentPrice = parseInt(cCoinData ? cCoinData[0]?.current_price : 0) 
+  const priceChangePercentage24h = cCoinData ? cCoinData[0]?.price_change_percentage_24h : 0
+  
+  return (
+    <div className="App">
+      <h1>{cName}</h1>
+      <p>current price: {currentPrice}</p>
+      <p>price change percentage 24h: {priceChangePercentage24h.toFixed(2)}%</p>
+      <h2>top 10 coins with the largest market capitalization</h2>
+      {cMarketCapData
+        ? cMarketCapData?.map((v, i) => {
+          if(i < 5)
+          {
+            return (
+              <p key = {i}>
+                {i + 1}. {v?.name} |current price: {v?.current_price}$ |market cap: {v?.market_cap}$
+              </p>
+            );
+          }
+          })
+        : "Loading..."}
+      <h2>top 7 trending coins</h2>
+      {cTrendingData
+        ? cTrendingData?.coins.map((v, i) => {
+            return (
+              <p key = {i}>
+                {i + 1}. {v?.item?.name} 
+              </p>
+            );
+          })
+        : "Loading..."}
+    </div>
+  );
 }
 
 export default App;
--- src/data/crpyto.js
+++ src/data/crpyto.js
@@ -1,48 +1,52 @@
 import { useEffect, useState } from "react"
 
 export default function Crpyto(cryptoName) {
-    const [currentPrice, SetCurrentPrice] = useState()
-    const [historicalData, SetHistoricalData] = useState()
+    const [coinData, SetCoinData] = useState()
+    //const [historicalData, SetHistoricalData] = useState()
     const [trendingCoins, SetTrendingCoins] = useState()
+    const [marketCapData, SetMarketCapData] = useState()
     //current price
     const vs_currencies = 'usd'
-    const currentPriceURL = 'https://api.coingecko.com/api/v3/simple/price?ids=' + cryptoName + '&vs_currencies=' + vs_currencies
-
+    //const currentPriceURL = 'https://api.coingecko.com/api/v3/simple/price?ids=' + crytpoName + '&vs_currencies=' + vs_currencies
+    const coinDataURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=' + vs_currencies + '&ids=' + cryptoName + '&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     //historical data
-    const currentDate = new Date()
-    const previousDate = currentDate.setDate(currentDate.getDate() - 1)
-    let day = new Date(previousDate).getDate() 
-    let month = new Date(previousDate).getMonth() 
-    let year = new Date(previousDate).getFullYear()
-    console.log(year)
+    //const currentDate = new Date()
+    //const previousDate = new Date(new Date().setDate(new Date().getDate() - 1))
+    //let day = new Date(previousDate).getDate() 
+    //let month = new Date(previousDate).getMonth() + 1
+    //let year = new Date(previousDate).getFullYear()
+    //console.log(day, month, year)
 
-    const historicalDataURL = 'https://api.coingecko.com/api/v3/coins/' + cryptoName + '/history?date=' + day + '-' + month + '-' + year;
+  
+
+    //const historicalDataURL = 'https://api.coingecko.com/api/v3/coins/' + cryptoName + '/history?date=' + day.toString() + '-' + month.toString() + '-' + year.toString();
     
     //trending coins
     const trendingURL = 'https://api.coingecko.com/api/v3/search/trending'
 
     //market cap
+    const marketCapURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
     useEffect(() => {
         const f1 = async () => {
-            const res = await fetch(currentPriceURL)
+            const res = await fetch(coinDataURL)
             const d = await res.json()
-            SetCurrentPrice(d)
+            SetCoinData(d)
         }
         f1()
         const f2 = async () => {
-            const res = await fetch(historicalDataURL)
-            const d = await res.json()
-            SetHistoricalData(d)
-        }
-        f2()
-        const f3 = async () => {
             const res = await fetch(trendingURL)
             const d = await res.json()
             SetTrendingCoins(d)
         }
+        f2()
+        const f3 = async () => {
+            const res = await fetch(marketCapURL)
+            const d = await res.json()
+            SetMarketCapData(d)
+        }
         f3()
     }, [])
 
-    return [currentPrice, historicalData, trendingCoins]
+    return [coinData, trendingCoins, marketCapData]
 }

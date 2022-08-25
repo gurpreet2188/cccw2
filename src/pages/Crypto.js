9f14c2e994ef08e24aab3b563909e69848fdf202
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2022-08-25 17:07:50 +0800

    fix typo

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -85,7 +85,7 @@ function Crypto() {
 
   return (
     <div className='flex flex-col items-center space-y-4 p-2'>
-      <h1  className='self-start text-[28px] tracking-wider font-light'>More News</h1>
+      <h1  className='self-start text-[28px] tracking-wider font-light'>Crypto</h1>
       <div className='w-[100%] border border-black/10 rounded-md'>
         {
           (primaryList && primaryListEntries && header) ? listCommon() : 'Loading...'
